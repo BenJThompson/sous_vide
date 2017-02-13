670f951ad18a2b740114b57a348b8bdf870d4050
Benjamin Thompson <benjamin.thompson@fkie.fraunhofer.de>
2017-02-13 17:12:13 +0100

    Hello World

---
+++ sous_vide.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: thompson
+ *
+ * Created on February 13, 2017, 4:46 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ sous_vide.X/nbproject/main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: thompson
+ *
+ * Created on February 13, 2017, 5:03 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
